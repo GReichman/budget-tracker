73865f1fd878cd7fee6b07fb3f4c17952b808ea1
Garrett Reichman <gsreichman@gmail.com>
2020-04-11 19:03:41 -0400

    added hosted mongo uri

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = 3005;
-
+var MONDODB_URI = process.env.MONDODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONDODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
